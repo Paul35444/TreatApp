eab9fb35c160b6e928c2a834a19e768f8da64c75
Paul <paul35444@gmail.com>
2018-01-14 11:04:25 -0600

    Login to bottom of form with jQuery

---
+++ Treat/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+
+    var itemForm = $("#itemForm")
+    itemForm.hide();
+
+    var button = $("#itemFormButton");
+    button.on("click",
+        function() {
+            console.log("Submitting Form");
+        });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $("#.popup-form");
+
+    $loginToggle.on("click",
+        function() {
+            $popupForm.fadeToggle(1000);
+        });
+
+});
